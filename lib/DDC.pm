99340142c5b5a45661dd4468ab6a57e745ab3e0b
Torsten Raudssus <torsten@raudssus.de>
2014-12-07 17:53:19 +0000

    Import of GETTY/Data-Coloured-0.001.tar.gz from CPAN.
    gitpan-cpan-distribution: Data-Coloured
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         GETTY/Data-Coloured-0.001.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

---
+++ lib/DDC.pm
@@ -0,0 +1,59 @@
+package DDC;
+BEGIN {
+  $DDC::AUTHORITY = 'cpan:GETTY';
+}
+# ABSTRACT: Visualize random bytes
+$DDC::VERSION = '0.001';
+use strict;
+use warnings;
+use parent 'Data::Coloured';
+
+our @EXPORT_OK = qw();
+
+our @EXPORT = qw( pc c coloured poloured );
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+DDC - Visualize random bytes
+
+=head1 VERSION
+
+version 0.001
+
+=head1 DESCRIPTION
+
+See L<Data::Coloured>
+
+=head1 SUPPORT
+
+IRC
+
+  Join #vonbienenstock on irc.freenode.net. Highlight Getty for fast reaction :).
+
+Repository
+
+  http://github.com/Getty/p5-data-coloured
+  Pull request and additional contributors are welcome
+
+Issue Tracker
+
+  http://github.com/Getty/p5-data-coloured/issues
+
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudss.us>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Torsten Raudssus.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
